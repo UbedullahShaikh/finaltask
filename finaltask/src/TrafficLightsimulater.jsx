ee0e9848de5417f8b9ccd2e9b1f052dcab7496c0
UbedullahShaikh <shykhubedullah@gmail.com>
2025-06-03 11:42:37 +0500

    convert to functional React Component

--- finaltask/src/TrafficLightsimulater.jsx
+++ finaltask/src/TrafficLightsimulater.jsx
@@ -2,25 +2,29 @@ import React from 'react';
 
 
 export default function TrafficLightsimulater() {
-   
+    const [currentLight, setCurrentLight] = React.useState('red');
+    const lights = ['light red', 'light yellow', 'light green'];
+    React.useEffect(() => {
+       
+    }, [lights]);
+
   return (
     <>
       <div>
         <h1>Traffic Light Simulation</h1>
         <div>
             <div className="traffic-light">
-                <div className='light red'></div>
-                <div className='light yellow'></div>
-                <div className='light green'></div>
+                <div className={currentLight}></div>
+                <div className={currentLight}></div>
+                <div className={currentLight}></div>
             </div>
             <style>{`
                 .traffic-light {
                 display: flex;
-                justify-content: self-center;
                 flex-direction: column;
                 width: 100px;
                 height: 300px;
-                background-color: black;
+                background-color: lightgray;
                 border-radius: 10px;
                 padding: 10px;
                 }
