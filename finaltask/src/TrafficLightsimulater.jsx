17ee722d314221077c592bcbcde0d80057d171fa
UbedullahShaikh <shykhubedullah@gmail.com>
2025-06-03 12:09:16 +0500

    enhance ui final commit

--- finaltask/src/TrafficLightsimulater.jsx
+++ finaltask/src/TrafficLightsimulater.jsx
@@ -1,37 +1,57 @@
-import React from 'react';
-
+import React from "react";
 
 export default function TrafficLightsimulater() {
-    const [currentLight, setCurrentLight] = React.useState('red');
-    const light = ['light red', 'light yellow', 'light green'];
-    React.useEffect(() => {
-        const interval = setInterval(() => {
-            setCurrentLight(prev => {
-                const current = light.indexOf(prev);
-                const next = (current + 1) % light.length;
-                return light[next];
-            });
-        }, 3000);
-        return () => clearInterval(interval);
-    }, [light]);
+  const [currentLight, setCurrentLight] = React.useState("red");
+  const light = ["light red", "light yellow", "light green"];
+  React.useEffect(() => {
+    const interval = setInterval(() => {
+      setCurrentLight((prev) => {
+        const current = light.indexOf(prev);
+        const next = (current + 1) % light.length;
+        return light[next];
+      });
+    }, 3000);
+    return () => clearInterval(interval);
+  }, [light]);
 
   return (
     <>
-      <div>
-        <h1>Traffic Light Simulation</h1>
+      <div className="outter">
+        <h1 className="heading">Traffic Light Simulation</h1>
         <div>
-            <div className="traffic-light">
-                <div className={currentLight=="light red"? currentLight: "light"}></div>
-                <div className={currentLight=="light yellow"? currentLight: "light"}></div>
-                <div className={currentLight=="light green"? currentLight: "light"}></div>
-            </div>
-            <style>{`
+          <div className="traffic-light">
+            <div
+              className={currentLight == "light red" ? currentLight : "light gray"}
+            ></div>
+            <div
+              className={
+                currentLight == "light yellow" ? currentLight : "light gray"
+              }
+            ></div>
+            <div
+              className={currentLight == "light green" ? currentLight : "light gray"}
+            ></div>
+          </div>
+          <style>{`
+            .heading{
+                color: white;
+            }
+            .outter{
+                display: flex;
+                justify-content: center;
+                flex-direction: column;
+                padding: 20px;
+                margin: 20px;
+                border: 1px solid black;
+                background: black;
+                
+            }
                 .traffic-light {
                 display: flex;
                 flex-direction: column;
                 width: 100px;
                 height: 300px;
-                background-color: lightgray;
+                background-color: gray;
                 border-radius: 10px;
                 padding: 10px;
                 }
@@ -43,13 +63,19 @@ export default function TrafficLightsimulater() {
                 opacity: 0.3;
                 }
                 .red {
-                background-color: red;
+                background-color: darkred;
+                opacity: 1;
                 }
                 .yellow {
                 background-color: yellow;
+                 opacity: 1;
                 }
                 .green {
                 background-color: green;
+                 opacity: 1;
+                }
+                .gray{
+                background-color: darkgray;
                 }
             `}</style>
         </div>
