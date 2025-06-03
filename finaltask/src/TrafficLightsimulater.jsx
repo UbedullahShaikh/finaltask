eb24968039aab0d4bfa53ea1ffdd8a73cabedcb8
UbedullahShaikh <shykhubedullah@gmail.com>
2025-06-03 11:52:10 +0500

    implement logic life cycle logic using useeffect

--- finaltask/src/TrafficLightsimulater.jsx
+++ finaltask/src/TrafficLightsimulater.jsx
@@ -3,10 +3,17 @@ import React from 'react';
 
 export default function TrafficLightsimulater() {
     const [currentLight, setCurrentLight] = React.useState('red');
-    const lights = ['light red', 'light yellow', 'light green'];
+    const light = ['light red', 'light yellow', 'light green'];
     React.useEffect(() => {
-       
-    }, [lights]);
+        const interval = setInterval(() => {
+            setCurrentLight(prev => {
+                const current = light.indexOf(prev);
+                const next = (current + 1) % light.length;
+                return light[next];
+            });
+        }, 3000);
+        return () => clearInterval(interval);
+    }, [light]);
 
   return (
     <>
@@ -14,9 +21,9 @@ export default function TrafficLightsimulater() {
         <h1>Traffic Light Simulation</h1>
         <div>
             <div className="traffic-light">
-                <div className={currentLight}></div>
-                <div className={currentLight}></div>
-                <div className={currentLight}></div>
+                <div className={currentLight=="light red"? currentLight: "light"}></div>
+                <div className={currentLight=="light yellow"? currentLight: "light"}></div>
+                <div className={currentLight=="light green"? currentLight: "light"}></div>
             </div>
             <style>{`
                 .traffic-light {
